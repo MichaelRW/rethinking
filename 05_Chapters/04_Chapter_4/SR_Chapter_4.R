cdba27cfc2b8c7ffe94035dc6ce1fc05cd91bca1
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2020-06-04 21:58:06 -0400

    a.) Continued review of Chapter 4 code.
    Michael R. Wirtzfeld on Thursday, June 4, 2020 at 21:58 EST

--- 05_Chapters/04_Chapter_4/SR_Chapter_4.R
+++ 05_Chapters/04_Chapter_4/SR_Chapter_4.R
@@ -213,11 +213,13 @@ library(rethinking)
 data(Howell1); d <- Howell1; d2 <- d[ d$age >= 18 , ]
 plot( d2$height ~ d2$weight )
 
+
 ## R Code 4.38
 set.seed(2971)
 N <- 100                   # 100 lines
 a <- rnorm( N , 178 , 20 )
 b <- rnorm( N , 0 , 10 )
+
 
 ## R Code 4.39
 plot( NULL , xlim=range(d2$weight) , ylim=c(-100,400) ,
@@ -230,6 +232,7 @@ for ( i in 1:N ) curve( a[i] + b[i]*(x - xbar) ,
                         from=min(d2$weight) , to=max(d2$weight) , add=TRUE ,
                         col=col.alpha("black",0.2) )
 
+
 ## R Code 4.40
 b <- rlnorm( 1e4 , 0 , 1 )
 dens( b , xlim=c(0,5) , adj=0.1 )
@@ -240,10 +243,12 @@ N <- 100                   # 100 lines
 a <- rnorm( N , 178 , 20 )
 b <- rlnorm( N , 0 , 1 )
 
+
 ## R Code 4.42
 # load data again, since it's a long way back
 library(rethinking)
 data(Howell1); d <- Howell1; d2 <- d[ d$age >= 18 , ]
+
 
 # define the average weight, x-bar
 xbar <- mean(d2$weight)
