6b6a24f640107a70bca347224777b411d518d587
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2020-06-16 09:32:24 -0400

    a.) Complete questions 4M2 and 4M3. b.) Start question 4M4.
    Michael R. Wirtzfeld on Tuesday, June 16, 2020 at 9:32 EST

--- 05_Chapters/04_Chapter_4/SR_Chapter_4_Questions_June_15_2020.R
+++ 05_Chapters/04_Chapter_4/SR_Chapter_4_Questions_June_15_2020.R
@@ -33,16 +33,53 @@
 # See page 93.
 
 
-## Q6 - Medium (Monday, June 15, 2020 at 7:25 EST)
+## 4M1 - Medium (Monday, June 15, 2020 at 7:25 EST)
 #
 library(rethinking)
 
 curve( dnorm(x, 0, 10), from=-50, to=50 )  # Distribution for mean (mu).
 curve( dexp(x, rate=1, log=FALSE), from=-2, to=10)  # Distribution for standard deviatin (sigma).
 
-N <- 1e4
+N <- 1e4; set.seed(2971)  # Arbitrary value for seed (see page 95 of textbook).
 sample_mu <- rnorm(N, 0, 10)
 sample_sigma <- rexp(N, rate=1)
 
 prior_h <- rnorm(N, sample_mu, sample_sigma)
 dens(prior_h)
+precis(prior_h); boxplot(prior_h); plot(prior_h)
+
+
+## 4M2 - Medium (Tuesday, June 16, 2020 at 9:15 EST)
+#
+# See page 88, R Code 4.27 to 4.29
+#
+flist <- alist(
+     height ~ dnorm( mu , sigma ) ,
+     mu ~ dnorm( mean=0 , std10 ) ,
+     sigma ~ dexp( rate=1 )
+)
+
+
+## 4M3 - Medium (Tuesday, June 16, 2020 at 9:22 EST)
+#
+# See question 4E4.
+#
+# yi ~ Normal(mu, sigma)
+# mui = a + b*xi
+# a ~ Normal(0, 10)
+# b ~ Uniform(0, 1)
+# sigma ~ Exponential(1)
+
+
+## 4M4 - Medium (Tuesday, June 16, 2020 at 9:26 EST)
+#
+# Data
+# Year 1 -> S1_height, S2_height, ... SN_height
+# Year 2 -> S1_height, S2_height, ... SN_height
+# Year 3 -> S1_height, S2_height, ... SN_height
+#
+# 
+
+
+
+
--- ReadMe_MRW_May_31_2020.txt
+++ ReadMe_MRW_May_31_2020.txt
@@ -5,10 +5,17 @@ rm(list=ls()) -> Clear variables from environment.
 
 
 =================================================
+Tuesday, June 16, 2020 Fold
+
+Complete questions 4M2 and 4M3.  Start question 4M3.
+
+EndFold
+
+
+=================================================
 Sunday, May 31, 2020 Fold
 
 Review code for Chapter 4.
-
 
 EndFold
 
